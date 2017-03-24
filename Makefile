dbf6f09bcbdcdeedb4307b8a52fa994c6ee17ef9
Pedram Pejman <pedrampejman2010@gmail.com>
2017-03-24 16:53:40 -0400

    Added basic RPC server to GEN

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
+GEN_PROTOS=./gen/src/protos
+
 all: build
 
 build: protos
 
 protos: .FORCE
+	# Copy proto files for app and fe to be dynamically compilation
 	echo fe app | xargs -n 1 cp -rf protos
+	# Pre-compile genius proto
+	python -m grpc_tools.protoc -I ./protos --python_out=$(GEN_PROTOS) --grpc_python_out=$(GEN_PROTOS) ./protos/genius.proto
 
 network:
 	sudo ./bash/network.sh
---
+++ gen/src/server.py
@@ -0,0 +1,38 @@
+from concurrent import futures
+import time
+import os
+
+import grpc
+
+import protos.genius_pb2 as genius
+import protos.genius_pb2_grpc as genius_grpc
+
+ONE_DAY = 60 * 60 * 24
+GEN_RPC_PORT = os.getenv('GEN_RPC_HOST', '35000')
+
+class Handshake(genius_grpc.HandshakeServicer):
+    """Implementation of Handshake RPC server"""
+
+    def Shake(self, request, context):
+        """Implements the Shake test service RPC handler"""
+
+        print("Received HandshakeRequest with name %s" %(request.name))
+        return genius.HandshakeResponse(message="Hello, %s!" % request.name)
+
+def main():
+    """Sets up the server and listens on GEN_RPC_PORT"""
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    genius_grpc.add_HandshakeServicer_to_server(Handshake(), server)
+    server.add_insecure_port("[::]:" + GEN_RPC_PORT)
+    server.start()
+    print("Server starting on port %s" %(GEN_RPC_PORT))
+    
+    try:
+        while True:
+            time.sleep(ONE_DAY)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if (__name__ == "__main__"):
+    main()
