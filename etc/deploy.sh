15075b745251457adaf24ee16ad03a3e96e6f2cf
Pedram Pejman <pedrampejman2010@gmail.com>
2017-04-11 16:15:07 -0400

    Fixed automatated deployment bug

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-echo "Building the Application image..."
-docker build -t ${AUTHOR}/${APP_IMAGE_NAME}:$TRAVIS_COMMIT -f ${APP_DIR}/Dockerfile ${APP_DIR}
-
-echo "Building the Frontend image..."
-docker build -t ${AUTHOR}/${FE_IMAGE_NAME}:$TRAVIS_COMMIT -f ${FE_DIR}/Dockerfile ${FE_DIR}
-
-echo "Building the Genius image..."
-docker build -t ${AUTHOR}/${GEN_IMAGE_NAME}:$TRAVIS_COMMIT -f ${GEN_DIR}/Dockerfile ${GEN_DIR}
-
 echo "Creating gcloud service key..."
 echo ${GCLOUD_SERVICE_KEY_PRD} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 
@@ -27,19 +18,19 @@ echo "Logging to docker as $DOCKER_USERNAME..."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 echo "Pushing Docker images to repository.."
-docker push ${AUTHOR}/${APP_IMAGE_NAME}:$TRAVIS_COMMIT
-docker push ${AUTHOR}/${FE_IMAGE_NAME}:$TRAVIS_COMMIT
-docker push ${AUTHOR}/${GEN_IMAGE_NAME}:$TRAVIS_COMMIT
+docker push ${AUTHOR}/${APP_IMAGE_NAME}:$TAG
+docker push ${AUTHOR}/${FE_IMAGE_NAME}:$TAG
+docker push ${AUTHOR}/${GEN_IMAGE_NAME}:$TAG
 
 kubectl config view
 kubectl config current-context
 
 echo "Deploying container images to kubernetes..."
 kubectl set image deployment/${APP_DEPLOYMENT_NAME} \
-  ${APP_CONTAINER_NAME}=${AUTHOR}/${APP_IMAGE_NAME}:$TRAVIS_COMMIT
+  ${APP_CONTAINER_NAME}=${AUTHOR}/${APP_IMAGE_NAME}:$TAG
 kubectl set image deployment/${FE_DEPLOYMENT_NAME} \
-  ${FE_CONTAINER_NAME}=${AUTHOR}/${FE_IMAGE_NAME}:$TRAVIS_COMMIT
+  ${FE_CONTAINER_NAME}=${AUTHOR}/${FE_IMAGE_NAME}:$TAG
 kubectl set image deployment/${GEN_DEPLOYMENT_NAME} \
-  ${GEN_CONTAINER_NAME}=${AUTHOR}/${GEN_IMAGE_NAME}:$TRAVIS_COMMIT
+  ${GEN_CONTAINER_NAME}=${AUTHOR}/${GEN_IMAGE_NAME}:$TAG
 
 echo "Finished deployment"
