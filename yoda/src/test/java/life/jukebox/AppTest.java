6a6bfa9d9d990f7bbc856e8152d531972ad89481
Peyman Pejman <pp2df@virginia.edu>
2017-12-27 20:21:16 -0800

    Fixed AppTest.java spacing

--- yoda/src/test/java/life/jukebox/AppTest.java
+++ yoda/src/test/java/life/jukebox/AppTest.java
@@ -1,4 +1,4 @@
-package life.jukebox;
+package life.jukebox.yoda;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -8,31 +8,31 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+  extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+  /**
+   * Create the test case
+   *
+   * @param testName name of the test case
+   */
+  public AppTest( String testName )
+  {
+    super( testName );
+  }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  /**
+   * @return the suite of tests being tested
+   */
+  public static Test suite()
+  {
+    return new TestSuite( AppTest.class );
+  }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+  /**
+   * Rigourous Test :-)
+   */
+  public void testApp()
+  {
+    assertTrue( true );
+  }
 }
