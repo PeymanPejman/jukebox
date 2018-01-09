4df8bdb6ab3a7a4ca84a800d4e4f21eef8e0401c
Peyman Pejman <pp2df@virginia.edu>
2018-01-09 14:11:46 -0800

    Implemented YodaSubscriber and MessageReceiver

---
+++ yoda/src/main/java/life/jukebox/YodaSubscriberClient.java
@@ -0,0 +1,35 @@
+package life.jukebox.yoda;
+
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
+import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.SubscriptionName;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class YodaSubscriberClient{
+  private static final String PROJECT_ID = getEnvironmentVariable("GCP_PROJECT_ID");
+  private static final String SUBSCRIPTIONID = getEnvironmentVariable("PUBSUB_SUB_JUKEBOX_CREATED_YODA");
+
+    private static String getEnvironmentVariable (String environmentKey){
+      if (System.getenv(environmentKey) == null) { 
+        throw new IllegalArgumentException("Environment variable " + environmentKey + " does not exist");
+      }
+      return System.getenv(environmentKey);
+    }
+  public static void main(String[] args){
+    SubscriptionName subscriptionName = SubscriptionName.of(PROJECT_ID, SUBSCRIPTIONID);
+    Subscriber subscriber = null;
+    try{
+      subscriber = Subscriber.newBuilder(subscriptionName, new JukeboxCreatedMessageReceiver()).build();
+
+      subscriber.startAsync().awaitRunning();
+    }finally {
+      if (subscriber != null) {
+        subscriber.stopAsync();
+      }
+    }
+  }
+}
