2ed45727895b5e32bdee4f5ca469978c58d9ab87
Peyman Pejman <pp2df@virginia.edu>
2018-01-20 20:27:37 -0800

    fixed tabbing

--- jb-yoda/src/main/java/life/jukebox/JBWatcher.java
+++ jb-yoda/src/main/java/life/jukebox/JBWatcher.java
@@ -1,22 +1,22 @@
 import java.util.concurrent.*;
 
 public class JBWatcher implements Runnable{
-	private ScheduledFuture<?> watcherHandle;
-	private String jbId;
+  private ScheduledFuture<?> watcherHandle;
+  private String jbId;
 
-	public JBWatcher(String jbId) {
-		this.jbId = jbId;
-	}
+  public JBWatcher(String jbId) {
+    this.jbId = jbId;
+  }
 
-	public ScheduledFuture<?> getWatcherHandle(){
-		return watcherHandle;
-	} 
+  public ScheduledFuture<?> getWatcherHandle(){
+    return watcherHandle;
+  } 
 
-	public void setWatcherHandle(ScheduledFuture<?> watcherHandle){
-		this.watcherHandle = watcherHandle;
-	}
+  public void setWatcherHandle(ScheduledFuture<?> watcherHandle){
+    this.watcherHandle = watcherHandle;
+  }
 
-	public void run(){
-		System.out.println("Spotify API call to get current playing track");
-	}
+  public void run(){
+    System.out.println("Spotify API call to get current playing track");
+  }
 }
--- jb-yoda/src/main/java/life/jukebox/JBWatcherManager.java
+++ jb-yoda/src/main/java/life/jukebox/JBWatcherManager.java
@@ -2,34 +2,33 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class JBWatcherManager {
-	private ScheduledExecutorService scheduler;
-	private Set<JBWatcher> activeWatchers;
-	private static JBWatcherManager manager;
-	private final long WATCHER_PERIOD = 10;
+  private ScheduledExecutorService scheduler;
+  private Set<JBWatcher> activeWatchers;
+  private static JBWatcherManager manager;
+  private final long WATCHER_PERIOD = 10;
 
-	protected JBWatcherManager() {
-		scheduler = Executors.newScheduledThreadPool(4);
-		activeWatchers = new HashSet<JBWatcher>();
-	}
+  protected JBWatcherManager() {
+    scheduler = Executors.newScheduledThreadPool(4);
+    activeWatchers = new HashSet<JBWatcher>();
+  }
 
-	public static JBWatcherManager getInstance() {
-		if (manager == null) {
-			manager = new JBWatcherManager();
-		}
-		return manager;
-	}
+  public static JBWatcherManager getInstance() {
+    if (manager == null) {
+      manager = new JBWatcherManager();
+    }
+    return manager;
+  }
 
-	public void addWatcher(JBWatcher watcher) {
-		if (!activeWatchers.add(watcher)) return;
-		ScheduledFuture<?> watcherHandle = scheduler.scheduleAtFixedRate(watcher, 0, WATCHER_PERIOD, TimeUnit.SECONDS);
-		watcher.setWatcherHandle(watcherHandle);
-	}
+  public void addWatcher(JBWatcher watcher) {
+    if (!activeWatchers.add(watcher)) return;
+    ScheduledFuture<?> watcherHandle = scheduler.scheduleAtFixedRate(watcher, 0, WATCHER_PERIOD, TimeUnit.SECONDS);
+    watcher.setWatcherHandle(watcherHandle);
+  }
 
-	public void killWatcher(JBWatcher watcher) {
-		if (!activeWatchers.remove(watcher)) return;
-		watcher.getWatcherHandle().cancel(false);
-	}
-
+  public void killWatcher(JBWatcher watcher) {
+    if (!activeWatchers.remove(watcher)) return;
+    watcher.getWatcherHandle().cancel(false);
+  }
   public boolean isWatcherActive(watcher) {
     return activeWatcher.contains(watcher);
   }
